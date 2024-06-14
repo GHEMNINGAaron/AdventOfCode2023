6ee3fbbe9549247899406c88fbcfa28cffd6af4b
GHEMNING Aaron <aaron.ghemning@institutsaintjean.com>
2024-06-14 23:07:59 +0100

    Day 2 part 2

--- Day2/cube_game/cube_game.py
+++ Day2/cube_game/cube_game.py
@@ -1,3 +1,5 @@
+from pygments.lexer import words
+
 with open('../puzzle_Input_Day2.txt', 'r') as file:
     words1 = file.readlines()
     words1 = [word.replace('\n', '') for word in words1]
@@ -13,16 +15,14 @@ def transform_game_in_dictionary(game):
     for i in range(len(dict_game[int(game[0])])):
         val = dict_game[int(game[0])][i].split(",")
         val = [c.replace(" ", "") for c in val]
-        print(val)
         val = {"".join(filter(str.isalpha, c)): int("".join(filter(str.isdigit, c))) for c in val}
-        print(val)
         values.append(val)
 
     dict_game[int(game[0])] = values
     return dict_game
 
-print(transform_game_in_dictionary("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"))
 
+print(transform_game_in_dictionary("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"))
 
 
 def verifiy_numbers_of_cube(dict):
@@ -48,4 +48,27 @@ def sum_of_id_of_possible_games(words):
     return sum
 
 
-print(sum_of_id_of_possible_games(words1))
+def fewest_number_of_cubes(dict):
+    fewest = {"blue": 0, "green": 0, "red": 0}
+    valeur = next(iter(dict.values()))
+    for i in range(len(valeur)):
+        key = [keys for keys in valeur[i] if keys in fewest.keys()]
+        for key in key:
+            if valeur[i][key] > fewest[key]:
+                fewest[key] = valeur[i][key]
+    return fewest
+
+
+def sum_of_power_of_a_set_of_games(words):
+
+    sum=0
+    for word in words:
+        power = 1
+        dict = transform_game_in_dictionary(word)
+        fewest = fewest_number_of_cubes(dict)
+        for fewest_value in fewest.values():
+            power *= fewest_value
+        sum += power
+    return sum
+
+print(sum_of_power_of_a_set_of_games(words1))
--- Day2/test/test_cube_game.py
+++ Day2/test/test_cube_game.py
@@ -1,7 +1,7 @@
 import pytest
 
 from Day2.cube_game.cube_game import (transform_game_in_dictionary, verifiy_numbers_of_cube,
-                                      sum_of_id_of_possible_games)
+                                      sum_of_id_of_possible_games, fewest_number_of_cubes, sum_of_power_of_a_set_of_games)
 def test_transform_string_in_dictionary():
     assert transform_game_in_dictionary("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green") == {1: [{'blue': 3, 'red': 4}, {'red': 1, 'green': 2, 'blue': 6}, {'green': 2}]}
     assert transform_game_in_dictionary('Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue') == {2: [{'blue': 1, 'green': 2}, {'green': 3, 'blue': 4, 'red': 1}, {'green': 1, 'blue': 1}]}
@@ -11,9 +11,23 @@ def test_verify_numbers_of_cube():
     assert verifiy_numbers_of_cube({1: [{'blue': 3, 'red': 4}, {'red': 1, 'green': 2, 'blue': 6}, {'green': 2}]}) == True
     assert verifiy_numbers_of_cube({4: [{'green': 1, 'red': 3, 'blue': 6}, {'green': 3, 'red': 6}, {'green': 3, 'blue': 15, 'red': 14}]}) == False
 
+
 def test_sum_of_id_of_possible_games():
     assert sum_of_id_of_possible_games(["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
                                         "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
                                         "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
                                         "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
                                         "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"]) == 8
+
+
+def test_fewest_number_of_cubes():
+    assert fewest_number_of_cubes({1: [{'blue': 3, 'red': 4}, {'red': 1, 'green': 2, 'blue': 6}, {'green': 2}]}) == {"blue":6, "red":4, "green":2}
+    assert fewest_number_of_cubes({4: [{'green': 1, 'red': 3, 'blue': 6}, {'green': 3, 'red': 6}, {'green': 3, 'blue': 15, 'red': 14}]}) == {"red":14, "green":3, "blue":15}
+
+
+def test_sum_of_power_of_a_set_of_games():
+    assert sum_of_power_of_a_set_of_games(["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                                           "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                                           "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                                           "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                                           "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"]) == 2286
